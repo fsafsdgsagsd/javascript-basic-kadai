554513706a31a543236a0ffb344bd398568579e9
fsafsdgsagsd <148539543+fsafsdgsagsd@users.noreply.github.com>
2023-11-09 18:26:28 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+let today = new Date();
+
+let year = today.getFullYear();
+let month = today.getMonth()+1;
+let day = today.getDate();
+console.log(year);
+console.log(month);
+console.log(day);
+
+console.log(year + '年',month + '月',day + '日');
